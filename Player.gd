e4d6244d3c5fc9b5f93ef580a88d55cda45db708
Fram446742 <francisco446742@gmail.com>
2024-11-22 12:02:10 +0100

    Working multiplayer

--- Player.gd
+++ Player.gd
@@ -9,11 +9,13 @@ signal health_changed(health_value)
 
 var health = 3
 
-const SPEED = 10.0
-const JUMP_VELOCITY = 10.0
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = 20.0
+@export var gravity = 20.0
+
+@export var JUMP_VELOCITY = 10.0
+@export var SPEED = 5.0
+@export var SHIFT_MULTIPLIER = 2.0
+
 
 func _enter_tree():
 	set_multiplayer_authority(str(name).to_int())
@@ -39,27 +41,27 @@ func _unhandled_input(event):
 			var hit_player = raycast.get_collider()
 			hit_player.receive_damage.rpc_id(hit_player.get_multiplayer_authority())
 
-func _physics_process(delta):
-	if not is_multiplayer_authority(): return
-	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
+func player_movement():
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("space") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var current_speed = SPEED
+	
+	if Input.is_action_pressed("shift"):
+		current_speed *= SHIFT_MULTIPLIER
+	
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
+
 
 	if anim_player.current_animation == "shoot":
 		pass
@@ -68,6 +70,15 @@ func _physics_process(delta):
 	else:
 		anim_player.play("idle")
 
+func _physics_process(delta):
+	if not is_multiplayer_authority(): return
+	
+	# Add the gravity.
+	#if not paused: 
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	player_movement()
 	move_and_slide()
 
 @rpc("call_local")
--- world.gd
+++ world.gd
@@ -5,33 +5,54 @@ extends Node
 @onready var hud = $CanvasLayer/HUD
 @onready var health_bar = $CanvasLayer/HUD/HealthBar
 
+var paused = false
 
 const Player = preload("res://player.tscn")
-const PORT = 9999
-var enet_peer = ENetMultiplayerPeer.new()
+const PORT = 3131
+const DEFAULT_SERVER_IP = "localhost"
+const USE_SSL = true # Set this to true or false based on your requirement 
+const TRUSTED_CHAIN_PATH = "res://certs/localhost.pem" 
+const PRIVATE_KEY_PATH = "res://certs/localhost-key.pem"
+#var enet_peer = WebSocketMultiplayerPeer.new()
+
+var connectedPlayers = {}
+
+var player_info = {"name": ""}
 
 func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		toggle_pause()
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 
 func _on_host_button_pressed():
+	var enet_peer = WebSocketMultiplayerPeer.new()
 	main_menu.hide()
 	hud.show()
 	
-	enet_peer.create_server(PORT)
+	#var priv = load(PRIVATE_KEY_PATH) 
+	#var cert = load(TRUSTED_CHAIN_PATH) 
+	#var tlsOptions = TLSOptions.server(priv, cert) 
+	
+	#enet_peer.create_server(PORT, "*", tlsOptions)
+	enet_peer.create_server(PORT, "*")
+	
 	multiplayer.multiplayer_peer = enet_peer
 	multiplayer.peer_connected.connect(add_player)
 	multiplayer.peer_disconnected.connect(remove_player)
 	
 	add_player(multiplayer.get_unique_id())
-	
-	upnp_setup()
 
 func _on_join_button_pressed():
+	var enet_peer = WebSocketMultiplayerPeer.new()
 	main_menu.hide()
 	hud.show()
 	
-	enet_peer.create_client(address_entry.text, PORT)
+	#var cert = load(TRUSTED_CHAIN_PATH) 
+	#var tlsOptions = TLSOptions.client(cert) 
+	
+	#enet_peer.create_client("wss://" + address_entry.text + ":" + str(PORT), tlsOptions)
+	enet_peer.create_client("ws://" + address_entry.text + ":" + str(PORT))
 	multiplayer.multiplayer_peer = enet_peer
 
 func add_player(peer_id):
@@ -53,18 +74,19 @@ func _on_multiplayer_spawner_spawned(node):
 	if node.is_multiplayer_authority():
 		node.health_changed.connect(update_health_bar)
 
-func upnp_setup():
-	var upnp = UPNP.new()
-	
-	var discover_result = upnp.discover()
-	assert(discover_result == UPNP.UPNP_RESULT_SUCCESS, \
-		"UPNP Discover Failed! Error %s" % discover_result)
+func toggle_pause():
+	paused = !paused
+	if paused:
+		$Pause_Menu.show()
+		$CanvasLayer.hide()
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	else:
+		$Pause_Menu.hide()
+		$CanvasLayer.show()
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-	assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), \
-		"UPNP Invalid Gateway!")
+func _on_quit_pressed() -> void:
+	get_tree().quit()
 
-	var map_result = upnp.add_port_mapping(PORT)
-	assert(map_result == UPNP.UPNP_RESULT_SUCCESS, \
-		"UPNP Port Mapping Failed! Error %s" % map_result)
-	
-	print("Success! Join Address: %s" % upnp.query_external_address())
+func _on_resume_pressed() -> void:
+	toggle_pause()
